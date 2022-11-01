96409a95823c384dd366ca830e13b4a96e1cb254
letsy1337 <skynightanddaY@gmail.com>
2022-11-01 03:24:50 +0200

    Update script.js

--- mod4-solution/js/script.js
+++ mod4-solution/js/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Yaakov", "Jeny", "Jen", "Jade", "Paul", "Frank", "Lobster", "Paula", "Laen", "Jakob"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
     
